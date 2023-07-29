100732e41a238a42c60e98d07d61423607415465
RGullit <gullit.profissional@gmail.com>
2023-07-29 16:06:56 -0300

    Primeira subida no git, arquivos realizados em estudos.

---
+++ calculoIMC.js
@@ -0,0 +1,16 @@
+function teste(){
+
+    resultado = prompt("Qual a sua idade?");
+    if (resultado >= 18) {
+        console.log ('Que legal! Você já pode ter a carteira de habilitação.')
+        
+        } else {
+            console.log('Poxa, que pena! Você ainda não pode ter a carteira de habilitação.')
+        
+            }
+
+   
+}
+
+teste();
+
---
+++ desafioMEDIA.js
@@ -0,0 +1,9 @@
+(function(){
+    function calcularMedia(){
+
+       if(valid){
+        window.alert("positivo")
+       }
+    }
+
+})()
